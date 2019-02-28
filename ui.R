972d5f27d5c136a8964e619cb27747cd63968018
ncurcio1 <ncurcio1@drew.edu>
2019-02-27 22:43:27 -0500

    Added in Tabs

--- ui.R
+++ ui.R
@@ -1,18 +1,26 @@
 library(shiny)
+
+#shiny IO library
 # install.packages("rhandsontable") # install the package
 library(rhandsontable) # load the package
 
 shinyUI( fluidPage(
-  fluidRow(
-    column(4,
-           helpText("editable table"),
-           rHandsontableOutput("table"),
-           br(),
-           actionButton("plotBtn","Plot")),
-           br(),
-           actionButton("curveBtn","curve"),
-    plotOutput("plot1")
-    
+  #sets up the tabs on the top
+  tabsetPanel( type = "tabs",
+               #names and adds input into the different panels
+               tabPanel( "plot",
+                         fluidRow(
+                           column(4,
+                                  helpText("editable table"),
+                                  rHandsontableOutput("table"),
+                                  br(),
+                                  actionButton("plotBtn","Plot"),
+                                  actionButton("curveBtn","curve"),
+                                  plotOutput("plot1")
+                           )
+                         )
+               ),
+               tabPanel("data")
+               
   )
-  
 ))
