1460f6330ecb4891de7f78283688d90e5d9cda45
ncurcio1 <ncurcio1@drew.edu>
2019-03-15 15:32:49 -0400

    no message

--- server.R
+++ server.R
@@ -2,10 +2,10 @@ library(shiny)
 library(rhandsontable)
 
 # Creating dataset
-#X = as.numeric(rep(NA, times = 10))
-#Y = as.numeric(rep(NA, times =  10))
-X <- c(0,1,2,5,20,80,100,300,500,1000)
-Y <- c(0,0.0154,0.02894,0.04534,0.07138,0.08778,0.08875,0.10032,0.0955,0.06752)
+X = as.numeric(rep(NA, times = 10))
+Y = as.numeric(rep(NA, times =  10))
+#X <- c(0,1,2,5,20,80,100,300,500,1000)
+#Y <- c(0,0.0154,0.02894,0.04534,0.07138,0.08778,0.08875,0.10032,0.0955,0.06752)
 df1 = data.frame(X=X, Y=Y)
 
 
@@ -24,7 +24,7 @@ shinyServer(function(input,output,session){
   observeEvent(input$entr,{
     X = as.numeric(rep(NA, times = input$obs))
     Y = as.numeric(rep(NA, times = input$obs))
-    #df1 = data.frame(X=X, Y=Y)
+    df1 = data.frame(X=X, Y=Y)
     output$table <- renderRHandsontable({
       rhandsontable(df1) # converts the R dataframe to rhandsontable object
     })})
