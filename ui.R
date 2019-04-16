87e89485aed8256735d4ececd845dd01ae96a170
isawicki <33033505+isawicki@users.noreply.github.com>
2019-04-15 22:10:50 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,36 +11,32 @@ shinyUI( fluidPage(
                tabPanel( "Number of Data Sets", value = "panel1", 
                          fluidRow(
                            column(1,
-                           numericInput("obs", "Observations:", 10, min = 1, max = 100),
-                           verbatimTextOutput("value"),
-                           actionButton("entr", "Enter")
-                         )),
-                        fluidRow(
-                          column(6,
-                                 helpText("Editable Table"),
-                                 rHandsontableOutput("table"),
-                                 textInput("title", "Title", "Title"),
-                                 textInput("xaxis", "X-axis lable", "X-Axis"),
-                                 textInput("yaxis", "Y-axis lable", "Y-Axis"),
-                                 numericInput("vmax", "Vmax Estimate", 1),
-                                 numericInput("Km", "Km Estimate", 1),
-                                 actionButton("plotBtn", "Plot"),
-                                 actionButton("curveBtn","Curve")
-                          )
-                        )
+                                  numericInput("obs", "Observations:", 10, min = 1, max = 100),
+                                  verbatimTextOutput("value"),
+                                  actionButton("entr", "Enter")
+                           )),
+                         fluidRow(
+                           column(6,
+                                  helpText("Editable Table"),
+                                  rHandsontableOutput("table"),
+                                  textInput("title", "Title", "Title"),
+                                  textInput("xaxis", "X-axis lable", "X-Axis"),
+                                  textInput("yaxis", "Y-axis lable", "Y-Axis"),
+                                  numericInput("vmax", "Vmax Estimate", 1),
+                                  numericInput("Km", "Km Estimate", 1),
+                                  actionButton("plotBtn", "Plot"),
+                                  actionButton("curveBtn","Curve")
+                           )
+                         )
                ),
                tabPanel( "Plot", value = "panel3", 
                          plotOutput("plot1"),
                          downloadButton(outputId = "down", label = "Download the plot"),
-                         radioButtons(inputId = "var1", label = "Select the file type", choices = list("png", "pdf"))
-               ),
-               tabPanel("Km and Vmax", value = "panel3",
-                        h1("Km:",verbatimTextOutput("kmdisplay", placeholder = T)),
-                        h1("Vmax:",verbatimTextOutput("vmaxdisplay", placeholder = T)),
-                        h1("R", tags$sup(2),verbatimTextOutput("r2", placeholder = T))
-                        )
-                        
+                         radioButtons(inputId = "var1", label = "Select the file type", choices = list("png", "pdf")),
+                         h1("Km:",verbatimTextOutput("kmdisplay", placeholder = T)),
+                         h1("Vmax:",verbatimTextOutput("vmaxdisplay", placeholder = T)),
+                         h1("R", tags$sup(2),verbatimTextOutput("r2", placeholder = T))
+               )
   )
 )
 )
-
